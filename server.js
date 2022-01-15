958203bb3058808612e81e900d684ac1c1413715
sm3131 <sm2683@nau.edu>
2022-01-14 21:52:15 -0700

    created get routes to serve the note taker front end files

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.get('/', (req, res) => {
+    const fileName = __dirname + '/public/index.html'
+    res.sendFile(fileName);
+});
+
+app.get('/notes', (req, res) => {
+    const fileName = __dirname + '/public/notes.html'
+    res.sendFile(fileName);
 });
 
 app.listen(PORT, () => {
